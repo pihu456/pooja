fe8934a1c88169ffb6f95c39c5be4bc02fb22e5c
pihu456 <pihuu0123>
2018-01-30 10:05:53 +0530

    new file

---
+++ if.py
@@ -0,0 +1,60 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x= int(input("please enter the integer:"))
+please enter the integer:41
+>>> if x<0:
+	x=0
+	print("negative to zero")
+elif(x==0)
+SyntaxError: invalid syntax
+>>> x= int(input("please enter the integer:"))
+please enter the integer:41
+>>> if x<0:
+	x=0
+	print("negative to zero")
+elif(x==0):
+	print("zero")
+elif(x==1)
+SyntaxError: invalid syntax
+>>> x= int(input("please enter the integer:"))
+
+please enter the integer:41
+>>> if x<0:
+	x=0
+	print("negative to zero")
+elif(x==0):
+	print("zero")
+elif(x==1):
+	print("one")
+else:
+	print("more")
+
+	
+more
+>>> for i in range(5):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+>>> for i in range(5,10):
+
+
+	)
+	
+SyntaxError: invalid syntax
+>>> for i in range(5)
+SyntaxError: invalid syntax
+>>> for i in range(5):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+>>> 
